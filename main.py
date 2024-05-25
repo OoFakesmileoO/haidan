cc1efd350320ab28a479663ae90eaee8a5d41f4c
OoFakesmileoO <70942342+OoFakesmileoO@users.noreply.github.com>
2024-05-25 08:48:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -130,7 +130,6 @@ def main() :
         HEADERS = {
             'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36 Edg/125.0.0.0',
             'cookie': 'c_secure_login=' + _login + '; c_secure_uid=' + _uid + '; c_secure_pass=' + _pass + '; c_secure_tracker_ssl=' + _tracker_ssl + '; c_secure_ssl=' + _ssl,
-            'Upgrade-Insecure-Requests:': '1',
         }
 
         get_status()
