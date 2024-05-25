283d18a896f85e5bffb15800d644512579382127
OoFakesmileoO <70942342+OoFakesmileoO@users.noreply.github.com>
2024-05-25 08:43:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -130,7 +130,6 @@ def main() :
         HEADERS = {
             'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36 Edg/125.0.0.0',
             'cookie': 'c_secure_login=' + _login + '; c_secure_uid=' + _uid + '; c_secure_pass=' + _pass + '; c_secure_tracker_ssl=' + _tracker_ssl + '; c_secure_ssl=' + _ssl,
-            ':scheme:': 'https',
             'Accept:': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'Accept-Encoding:': 'gzip, deflate, br, zstd',
             'Accept-Language:': 'zh-CN,zh;q=0.9',
